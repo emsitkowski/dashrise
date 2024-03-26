83d6052144ace50cbf8ea66951acbaca1c435c7b
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-26 21:30:27 +0100

    feat: add user welcome section

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>index page</div>
+  <div>
+    <h1 class="text-3xl font-bold mb-3">Hi, {{ useSupabaseUser().value?.email?.split("@")[0] }} 👋</h1>
+    <p>Here's what's happening with your money this month</p>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
