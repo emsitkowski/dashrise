2c5dc2e91ca234c97a23ad5df658f67294d72c93
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-26 15:35:01 +0100

    feat: add new pages

---
+++ pages/analytics.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>analytics page</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ pages/vaults.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>vaults page</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
