3d05bfd6cc3c3aab50e16a829f30a19b4036813b
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-26 15:00:04 +0100

    feat: add new page

---
+++ pages/budget.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>budget page</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>index page</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
