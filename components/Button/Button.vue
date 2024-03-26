76ccc8a8d7109ca3e998c85d954d7b9c343ace48
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-26 21:08:00 +0100

    refactor: delete component

--- components/Button/Button.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <button class="inline-block px-3 py-4 bg-blue-500 text-white rounded-md">
-    <slot></slot>
-  </button>
-</template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
