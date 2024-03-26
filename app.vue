45ec77dd2d521fad2b2c946d8d40cfa04ac29d2a
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-26 15:52:55 +0100

    feat: container style tweaks

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="container max-w-5xl mx-auto font-sans text-dark-500">
+  <div class="bg-light-50 min-h-screen">
     <Navbar />
-    <NuxtPage />
+    <div class="container max-w-5xl mx-auto font-sans text-dark-500">
+      <NuxtPage />
+    </div>
   </div>
 </template>
