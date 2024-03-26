e6f9c1ad14f06981dfa1f0481f1f99109711c8d7
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-26 15:34:52 +0100

    feat: add basic responsiveness + small tweaks

--- components/Navbar/Navbar.vue
+++ components/Navbar/Navbar.vue
@@ -1,13 +1,11 @@
 <template>
-  <nav>
-    <ul
-      class="flex gap-6 py-6 border-b border-primary-8 [&_a]:opacity-30 [&_a]:px-4 [&_a]:py-3 [&_a]:rounded-md [&_a]:cursor-pointer"
-    >
+  <nav class="flex justify-between py-6 border-b border-primary-8">
+    <ul class="flex flex-row flex-wrap gap-4 sm:gap-6 [&_a]:opacity-30 [&_a]:rounded-md [&_a]:cursor-pointer">
       <li v-for="link in links" :key="link.label">
         <NuxtLink
           :to="link.path"
           :class="{ active: useRoute().path === link.path }"
-          class="[&.active]:bg-primary-50 [&.active]:text-primary-500 [&.active]:opacity-100"
+          class="block px-2 py-2 sm:px-4 sm:py-3 [&.active]:bg-primary-50 [&.active]:text-primary-500 [&.active]:opacity-100"
         >
           {{ link.label }}
         </NuxtLink>
@@ -18,26 +16,11 @@
 
 <script setup lang="ts">
 const links = [
-  {
-    path: "/",
-    label: "Dashboard",
-  },
-  {
-    path: "/budget",
-    label: "Budget",
-  },
-  {
-    path: "/vaults",
-    label: "Vaults",
-  },
-  {
-    path: "/activity",
-    label: "Activity",
-  },
-  {
-    path: "/analytics",
-    label: "Analytics",
-  },
+  { path: "/", label: "Dashboard" },
+  { path: "/budget", label: "Budget" },
+  { path: "/vaults", label: "Vaults" },
+  { path: "/activity", label: "Activity" },
+  { path: "/analytics", label: "Analytics" },
 ];
 </script>
 
