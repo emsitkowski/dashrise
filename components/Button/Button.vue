37fe21dfd7b2d7bd244b95f8f8c517dd5201fe0c
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-26 15:00:16 +0100

    feat: add button component

---
+++ components/Button/Button.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="inline-block px-3 py-4 bg-blue-500 text-white rounded-md">
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
