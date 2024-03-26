fe4736dbff404eddb3be53039c684bd51093b3b6
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-26 21:52:56 +0100

    feat: add simple page transitions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
   css: ["~/assets/style/main.scss"],
   modules: ["@nuxtjs/supabase"],
   supabase: { redirect: false }, // disable supabase default redirect rules
