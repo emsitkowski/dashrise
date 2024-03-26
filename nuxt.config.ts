0deb6e82ed63a7d954fc51a2e2516ddb4bd1c794
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-26 13:39:02 +0100

    feat: install tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: ["~/assets/style/main.scss"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
