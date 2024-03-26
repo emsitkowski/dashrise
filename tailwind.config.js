9c944c75431953157bf1d15ef5814a268dd8caf9
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-26 15:52:32 +0100

    feat: add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,12 +15,16 @@ export default {
     extend: {
       colors: {
         primary: {
+          4: "rgba(0, 0, 255, 0.04)",
           8: "rgba(0, 0, 255, 0.08)",
           50: "#EBEBFF",
           500: "#0101FF",
         },
         dark: {
           500: "#00001A",
+        },
+        light: {
+          50: "#f7f7f7",
         },
       },
     },
