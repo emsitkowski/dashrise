d21bbedb2ee0a9a511583cabce88652ef328c4c0
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-26 21:37:31 +0100

    style: add website horizontal padding

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
     id="app"
     class="relative font-sans bg-light-50 text-dark-500 after:content-[''] after:absolute after:w-full after:h-48 after:bg-gradient-to-t after:from-transparent after:to-primary-8% after:top-0 after:pointer-events-none after:select-none"
   >
-    <div class="container max-w-5xl min-h-screen mx-auto">
+    <div class="container max-w-5xl min-h-screen mx-auto px-4">
       <NuxtLayout>
         <NuxtPage class="pt-20" />
       </NuxtLayout>
