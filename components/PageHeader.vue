dfca489effac59cd98e271fcbb988421598048b3
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-27 10:16:03 +0100

    feat: add responsiveness to page header

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="flex justify-between items-start">
+  <header class="flex flex-col sm:flex-row gap-4 justify-between items-start">
     <div>
       <h1 class="text-3xl font-bold mb-3">{{ headerText }}</h1>
       <p v-if="subheaderText">{{ subheaderText }}</p>
