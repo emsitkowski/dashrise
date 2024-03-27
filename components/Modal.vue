fedcffabed92b2095109ecbd9fb5814cf5dcb5cf
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-27 16:10:18 +0100

    feat: add modal component

---
+++ components/Modal.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="fixed flex justify-center items-center top-0 left-0 right-0 w-full h-full px-4 bg-dark-80% z-50">
+    <!-- Modal card -->
+    <Card class="w-full max-w-lg">
+      <template #card-header>
+        <!-- Modal header -->
+        <span class="text-xl">{{ headerText }}</span>
+
+        <!-- Modal close button -->
+        <Button class="ml-6" variant="ghost" @click="$emit('close')">
+          <img src="~assets/icons/x.svg" alt="close" />
+        </Button>
+      </template>
+
+      <!-- Modal body -->
+      <template #card-body>
+        <slot></slot>
+      </template>
+    </Card>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps(["headerText"]);
+defineEmits(["close"]);
+</script>
+
+<style scoped></style>
