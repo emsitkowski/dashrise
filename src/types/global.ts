1141c0eff0647ee0042405ae4479b71e84daa4d5
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-29 18:38:03 +0100

    refactor: add new typescript interfaces

--- src/types/global.ts
+++ src/types/global.ts
@@ -1,6 +1,25 @@
+export interface Transaction {
+  id?: string;
+  category?: string;
+  date: string;
+  type: string;
+  name: string;
+  value: number | undefined;
+}
+
 export interface FormData {
   [name: string]: string; // Add index signature
   value: string;
+}
+
+export interface FormFields {
+  transactionType: string;
+  fields: {
+    [label: string]: string; // Add index signature
+    type: string;
+    name: string;
+    placeholder: string;
+  }[];
 }
 
 export interface Tab {
