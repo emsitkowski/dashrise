53243b740e2b558f39f360b19cead0296fe8fe6d
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-29 08:32:25 +0100

    fix: mobile browsers zooming on form interactions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
   ssr: false,
   app: {
     pageTransition: { name: "page", mode: "out-in" },
+    head: {
+      meta: [{ name: "viewport", content: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" }],
+    },
   },
   css: ["~/assets/style/main.scss"],
   modules: ["@nuxtjs/supabase"],
