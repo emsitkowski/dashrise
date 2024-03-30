5705e668a278c1163fc7e409f84c843370977e62
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-30 11:05:34 +0100

    feat: add component for transaction operations

---
+++ components/TransactionActionsModal.vue
@@ -0,0 +1,92 @@
+<template>
+  <Modal v-show="open" header-text="Add transaction" @close="$emit('close')">
+    <div class="flex flex-col gap-2">
+      <!-- Transaction type switch -->
+      <Tabs :items="tabs" @tab-switch="handleTabSwitch">
+        <!-- Transaction form -->
+        <Form class="mt-4" @submit="handleSubmit" :loading="isSubmitting" :state="formState" :schema="formSchema">
+          <!-- Date -->
+          <FormField label="Date">
+            <FormInput name="Date" type="date" placeholder="Pick transaction date" v-model="formState.date" />
+          </FormField>
+
+          <!-- Name -->
+          <FormField label="Name">
+            <FormInput name="Name" type="text" placeholder="Enter transaction name" v-model="formState.name" />
+          </FormField>
+
+          <!-- Category -->
+          <FormField v-show="activeTab.label === 'Expense'" label="Category">
+            <FormInput name="Name" type="text" placeholder="Enter transaction category" v-model="formState.category" />
+          </FormField>
+
+          <!-- Value -->
+          <FormField label="Value">
+            <FormInput name="Value" type="text" placeholder="Enter transaction value" v-model="formState.value" />
+          </FormField>
+
+          <!-- Submit button -->
+          <Button class="mt-4" ref="submitBtn" :loading="isSubmitting">Add transaction</Button>
+        </Form>
+      </Tabs>
+    </div>
+  </Modal>
+</template>
+
+<script setup lang="ts">
+import { incomeSchema, expenseSchema } from "~/src/schemas/form";
+import type { Tab, Transaction } from "~/src/types/global";
+
+const props = defineProps(["open"]);
+const emit = defineEmits(["close", "success"]);
+
+const isSubmitting = ref(false);
+const formState = ref<any>({});
+
+const tabs = [{ label: "Income" }, { label: "Expense" }];
+const activeTab = ref(tabs[0]); // Set initial active tab
+
+const formSchema = ref(incomeSchema); // set initial form schema
+
+async function handleSubmit() {
+  // Turn on loading state
+  toggleFormLoading();
+
+  // Add transaction type to form state
+  formState.value.type = activeTab.value.label;
+
+  // Save transaction
+  const success = await useTransactionStore().saveTransaction(formState.value as Transaction);
+
+  if (success) {
+    // Emit success event
+    emit("success");
+
+    // Clear form fields and form state
+    clearInputs();
+    formState.value = {};
+  } else {
+    throw Error("Failed to save transaction");
+  }
+
+  // Turn off loading state
+  toggleFormLoading();
+}
+
+function toggleFormLoading() {
+  isSubmitting.value = !isSubmitting.value;
+}
+
+function handleTabSwitch(tab: Tab) {
+  activeTab.value = tab;
+
+  // Set correct form schema
+  if (activeTab.value.label === "Income") {
+    formSchema.value = incomeSchema;
+  } else if (activeTab.value.label === "Expense") {
+    formSchema.value = expenseSchema;
+  }
+}
+</script>
+
+<style scoped></style>
