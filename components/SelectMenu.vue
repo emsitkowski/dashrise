4d5ded8e89acf5d6cb86a0be963b9835a4ec9144
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-30 12:39:28 +0100

    feat: add custom select component

---
+++ components/SelectMenu.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="relative flex flex-col cursor-pointer select-none" @click="handleClick">
+    <div class="select flex justify-between items-center px-3 py-2 bg-none border border-gray-300 rounded-md">
+      <div class="selected pointer-events-none">
+        <span ref="select">{{ selected }}</span>
+      </div>
+      <div class="icon pointer-events-none">
+        <img :class="{ 'rotate-180': isOpen }" src="~assets/icons/chevron-down.svg" alt="" />
+      </div>
+    </div>
+    <ul
+      class="absolute top-0 translate-y-10 w-full px-2 py-2 bg-white border border-gray-300 rounded-md max-h-64 overflow-auto z-50"
+      :class="{ hidden: !isOpen }"
+    >
+      <li v-for="option in options" class="hover:bg-dark-4% px-3 py-2 rounded">{{ option.label }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(["update:modelValue"]);
+const props = defineProps(["options"]);
+
+const select = ref();
+const selected = ref(props.options[0].label);
+const isOpen = ref(false);
+
+// Emit initial value
+emit("update:modelValue", props.options[0].label);
+
+function handleClick(e: Event) {
+  const target = e.target as HTMLElement;
+
+  // Change selected value
+  if (target.tagName === "LI" && target.textContent) {
+    selected.value = target.textContent;
+
+    // Close dropdown
+    isOpen.value = false;
+
+    // Emit event with selected value
+    emit("update:modelValue", target.textContent);
+  } else {
+    // Toggle dropdown
+    isOpen.value = !isOpen.value;
+  }
+}
+</script>
+
+<style scoped></style>
