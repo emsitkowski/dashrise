a526b6e7a9d1ce5d57b8d8a813ea8361ea7a9c34
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-30 10:41:36 +0100

    refactor: delete form label component

--- components/FormLabel.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <label class="font-medium" :for="inputID">{{ props.label }}</label>
-</template>
-
-<script setup lang="ts">
-const props = defineProps(["label", "inputID"]);
-</script>
-
-<style scoped></style>
