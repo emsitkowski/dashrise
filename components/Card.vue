9d73d3759d98e4da598bb8090f5b54641efaf3d3
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-30 14:06:48 +0100

    style: change padding

--- components/Card.vue
+++ components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="inline-block p-8 rounded-2xl bg-white">
+  <div class="inline-block p-6 sm:p-8 rounded-2xl bg-white">
     <!-- Card header -->
     <div :class="{ 'flex justify-between items-center mb-6 pb-6 border-b border-b-primary-8%': $slots['card-header'] }">
       <slot name="card-header"></slot>
