37397aa7f40157dab64eea582d9063edd7932c09
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-30 13:31:10 +0100

    feat: add utilities to enable and disable scrollbar

---
+++ utils/scrollbar.ts
@@ -0,0 +1,12 @@
+function disableScrollbar() {
+  const scrollBarWidth = window.innerWidth - document.documentElement.clientWidth;
+  document.documentElement.classList.add("overflow-hidden");
+  document.documentElement.style.paddingRight = scrollBarWidth + "px";
+}
+
+function enableScrollbar() {
+  document.documentElement.classList.remove("overflow-hidden");
+  document.documentElement.style.paddingRight = "0px";
+}
+
+export { disableScrollbar, enableScrollbar };
