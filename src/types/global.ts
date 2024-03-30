0e8fcc0d87bd64e147455b7c1cb809e5a7b1d4b7
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-30 11:03:47 +0100

    chore: remove unnecessary types

--- src/types/global.ts
+++ src/types/global.ts
@@ -7,21 +7,6 @@ export interface Transaction {
   value: number | undefined;
 }
 
-export interface FormData {
-  [name: string]: string; // Add index signature
-  value: string;
-}
-
-export interface FormFields {
-  transactionType: string;
-  fields: {
-    [label: string]: string; // Add index signature
-    type: string;
-    name: string;
-    placeholder: string;
-  }[];
-}
-
 export interface Tab {
   label: string;
 }
