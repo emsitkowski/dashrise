7cb6f7af16ab2f43fa18e873d22a2aa4601358b7
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-30 11:04:58 +0100

    refactor: adjust modal instance

--- pages/index.vue
+++ pages/index.vue
@@ -7,11 +7,15 @@
       button-text="Add transaction"
       :button-icon="true"
       button-icon-type="plus"
+      @button-click="isModalOpen = true"
     ></PageHeader>
+
+    <TransactionModal :open="isModalOpen" @close="isModalOpen = false" @success="isModalOpen = false" />
   </div>
 </template>
 
 <script setup lang="ts">
+const isModalOpen = ref(false);
 const username = useSupabaseUser().value?.email?.split("@")[0];
 </script>
 
