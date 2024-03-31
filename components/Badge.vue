12ea10201f6e9651a068de6b3a4d5d12bf6e8c00
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-31 09:58:14 +0200

    feat: add badge component

---
+++ components/Badge.vue
@@ -0,0 +1,26 @@
+<template>
+  <span
+    class="flex justify-center items-center px-3 py-2 text-xs sm:text-sm rounded-md"
+    :class="{
+      'text-primary-500 bg-primary-4%': variant === 'primary',
+      'text-secondary-500 bg-secondary-4%': variant === 'secondary',
+    }"
+  >
+    {{ label }}
+  </span>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  variant: {
+    type: String,
+    default: "primary",
+  },
+});
+</script>
+
+<style scoped></style>
