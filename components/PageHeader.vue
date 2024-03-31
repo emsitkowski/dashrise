9687505d867e4ed2b7ff03daabb607413d9f860f
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-31 12:38:42 +0200

    style: add margin to page header

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="flex flex-col sm:flex-row gap-4 justify-between items-start">
+  <header class="flex flex-col sm:flex-row gap-4 justify-between items-start mb-11">
     <div>
       <h1 class="text-3xl font-bold mb-3">{{ headerText }}</h1>
       <p v-if="subheaderText">{{ subheaderText }}</p>
