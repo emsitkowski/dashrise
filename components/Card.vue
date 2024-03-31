edaa7cffe6f09e9f7d75d7fbc514ed21b989073a
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-31 09:28:22 +0200

    style: adjust padding and dividers

--- components/Card.vue
+++ components/Card.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="block p-4 sm:p-8 rounded-2xl bg-white overflow-x-auto">
+  <div class="block p-4 sm:p-8 rounded-2xl bg-white divide-y divide-primary-8% overflow-x-auto">
     <!-- Card header -->
-    <div :class="{ 'flex justify-between items-center mb-6 pb-6 border-b border-b-primary-8%': $slots['card-header'] }">
+    <div :class="{ 'flex justify-between items-center mb-6': $slots['card-header'] }">
       <slot name="card-header"></slot>
     </div>
 
@@ -9,7 +9,7 @@
     <slot name="card-body"></slot>
 
     <!-- Card footer -->
-    <div :class="{ 'mt-6 pt-6 border-t border-t-primary-8% text-center': $slots['card-footer'] }">
+    <div :class="{ 'mt-6 pt-6  text-center': $slots['card-footer'] }">
       <slot name="card-footer"></slot>
     </div>
   </div>
