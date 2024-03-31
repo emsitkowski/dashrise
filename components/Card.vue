c1e2e13d6e36947c0f27e457b7f8003f512093f5
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-31 22:01:19 +0200

    style: adjust layout and styling

--- components/Card.vue
+++ components/Card.vue
@@ -1,21 +1,20 @@
 <template>
-  <div class="block p-6 sm:p-8 rounded-2xl bg-white divide-y divide-primary-8% overflow-x-auto">
+  <div class="flex flex-col justify-center p-6 sm:p-8 rounded-2xl bg-white divide-y divide-primary-8% overflow-x-auto">
     <!-- Card header -->
     <div
       v-if="$slots['card-header']"
-      class="block"
       :class="{ 'flex justify-between items-center mb-6 sm:mb-8': $slots['card-header'] }"
     >
       <slot name="card-header"></slot>
     </div>
 
     <!-- Card body -->
-    <div v-if="$slots['card-body']" class="block">
+    <div class="pt-6" v-if="$slots['card-body']">
       <slot name="card-body"></slot>
     </div>
 
     <!-- Card footer -->
-    <div v-if="$slots['card-footer']" :class="{ 'mt-6 pt-6  text-center': $slots['card-footer'] }" class="block">
+    <div v-if="$slots['card-footer']" :class="{ 'mt-6 pt-6  text-center': $slots['card-footer'] }">
       <slot name="card-footer"></slot>
     </div>
   </div>
