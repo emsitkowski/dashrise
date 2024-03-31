33fb1faa6bc2f48fdfc2f5c89501cae357d4d969
Michał Sitkowski <emsitkowski@gmail.com>
2024-03-31 09:57:01 +0200

    fix: remove doubling dividers

--- components/Card.vue
+++ components/Card.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="block p-4 sm:p-8 rounded-2xl bg-white divide-y divide-primary-8% overflow-x-auto">
     <!-- Card header -->
-    <div :class="{ 'flex justify-between items-center mb-6': $slots['card-header'] }">
+    <div v-if="$slots['card-header']" :class="{ 'flex justify-between items-center mb-6': $slots['card-header'] }">
       <slot name="card-header"></slot>
     </div>
 
@@ -9,7 +9,7 @@
     <slot name="card-body"></slot>
 
     <!-- Card footer -->
-    <div :class="{ 'mt-6 pt-6  text-center': $slots['card-footer'] }">
+    <div v-if="$slots['card-footer']" :class="{ 'mt-6 pt-6  text-center': $slots['card-footer'] }">
       <slot name="card-footer"></slot>
     </div>
   </div>
