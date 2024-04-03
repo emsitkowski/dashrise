0e58c8831e5873669d27320306f3feb311b295c0
Michał Sitkowski <emsitkowski@gmail.com>
2024-04-03 23:15:41 +0200

    style: adjust padding

--- components/Card.vue
+++ components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col gap-6 p-4 sm:p-8 rounded-2xl bg-white overflow-x-auto">
+  <div class="flex flex-col gap-6 p-6 sm:p-8 rounded-2xl bg-white overflow-x-auto">
     <!-- Card header -->
     <div
       v-if="$slots['card-header']"
