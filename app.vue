6d76a1f8cc2211224ebe7238613e69b3d8cda40d
Michał Sitkowski <emsitkowski@gmail.com>
2024-04-03 14:34:09 +0200

    style: increase app container width

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
     id="app"
     class="relative after:content-[''] after:absolute after:w-full after:h-48 after:bg-gradient-to-t after:from-transparent after:to-primary-8% after:top-0 after:pointer-events-none after:select-none"
   >
-    <div class="container max-w-5xl min-h-screen mx-auto px-4">
+    <div class="container max-w-6xl min-h-screen mx-auto px-4">
       <NuxtLayout>
         <NuxtPage class="py-12 sm:py-20" />
       </NuxtLayout>
