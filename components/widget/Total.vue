0921bc16f5ad648de961e40e4501380c071767d1
Michał Sitkowski <emsitkowski@gmail.com>
2024-04-03 14:38:57 +0200

    feat: add total values widgets

---
+++ components/widget/Total.vue
@@ -0,0 +1,80 @@
+<template>
+  <Card class="relative">
+    <template #card-body>
+      <div class="flex flex-col gap-2 sm:gap-3">
+        <!-- Header -->
+        <div class="flex items-center gap-2 sm:gap-3">
+          <img v-if="icon === 'credit-card'" class="w-4 sm:w-5" src="~assets/icons/credit-card.svg" alt="" />
+          <img v-if="icon === 'arrow-in'" class="w-4 sm:w-5" src="~assets/icons/arrow-in.svg" alt="" />
+          <img v-if="icon === 'arrow-out'" class="w-4 sm:w-5" src="~assets/icons/arrow-out.svg" alt="" />
+          <img v-if="icon === 'box'" class="w-4 sm:w-5" src="~assets/icons/box.svg" alt="" />
+          <span class="text-xs sm:text-sm uppercase">{{ header }}</span>
+        </div>
+
+        <!-- Value -->
+        <div class="text-xl sm:text-3xl whitespace-nowrap shrink-0 overflow-hidden">
+          <span v-if="value">
+            <span class="opacity-0" ref="integer">0</span>
+            <span id="decimal" class="text-dark-32% text-xs sm:text-base"> PLN</span>
+          </span>
+          <div v-else>–</div>
+        </div>
+      </div>
+
+      <!-- Loading spinner -->
+      <SpinnerLoader
+        :label="`Calculating ${header}...`"
+        bg-color-class="bg-white"
+        stroke-color-class="stroke-dark-500"
+        :loading="loading"
+      />
+    </template>
+  </Card>
+</template>
+
+<script setup lang="ts">
+import anime from "animejs/lib/anime.es.js";
+const props = defineProps(["header", "icon", "value", "loading"]);
+
+const integer = ref();
+
+// Split value by decimal point
+const value = computed(() => {
+  return props.value;
+});
+
+watchEffect(() => {
+  animateValues();
+});
+
+// Animate values whenever they update
+function animateValues() {
+  if (integer.value) {
+    const startValue = revertCurrencyToNumber(integer.value.textContent);
+    const endValue = value.value;
+    const element = integer.value;
+    const isNegativeValue = endValue.toString().includes("-");
+
+    // Fade in values
+    anime({
+      targets: element,
+      opacity: 1,
+      duration: 800,
+      easing: "linear",
+    });
+
+    // Animate digits
+    anime({
+      targets: { value: startValue },
+      value: endValue,
+      duration: 1200,
+      easing: "linear",
+      update: function (animation: any) {
+        element.textContent = convertToCurrency(animation.animatables[0].target.value);
+      },
+    });
+  }
+}
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -11,25 +11,55 @@
       @button-click="isModalOpen = true"
     />
 
-    <!-- Transactions modal -->
-    <TransactionActionsModal :open="isModalOpen" @close="isModalOpen = false" @success="isModalOpen = false" />
+    <!-- Summary -->
+    <div class="grid grid-cols-2 xl:grid-cols-4 justify-between w-full [&>*]:w-full gap-4 sm:gap-8 mb-16">
+      <WidgetTotal
+        header="Balance"
+        icon="credit-card"
+        :value="getTotalTransactionValues.totalIncome - getTotalTransactionValues.totalExpenses"
+        :loading="useTransactionStore().loading"
+      />
+      <WidgetTotal
+        header="Income"
+        icon="arrow-in"
+        :value="getTotalTransactionValues.totalIncome"
+        :loading="useTransactionStore().loading"
+      />
+      <WidgetTotal
+        header="Expenses"
+        icon="arrow-out"
+        :value="getTotalTransactionValues.totalExpenses"
+        :loading="useTransactionStore().loading"
+      />
+      <WidgetTotal
+        header="Savings"
+        icon="box"
+        :value="getTotalTransactionValues.totalSavings"
+        :loading="useTransactionStore().loading"
+      />
+    </div>
 
     <!-- Recent activity widget -->
-    <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 mt-12">
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-8">
       <div class="flex flex-col">
         <SectionHeader></SectionHeader>
         <WidgetRecentActivity
-          :transactions="useTransactionStore().filterTransactionsByDate('2024', '03', 10)"
+          :transactions="useTransactionStore().filterTransactionsByDate('2024', '04', 6)"
           :loading="useTransactionStore().loading"
         />
       </div>
     </div>
+
+    <!-- Transactions modal -->
+    <TransactionActionsModal :open="isModalOpen" @close="isModalOpen = false" @success="isModalOpen = false" />
   </div>
 </template>
 
 <script setup lang="ts">
 const isModalOpen = ref(false);
 const username = useSupabaseUser().value?.email?.split("@")[0];
+
+const { getTotalTransactionValues } = storeToRefs(useTransactionStore());
 </script>
 
 <style scoped></style>
