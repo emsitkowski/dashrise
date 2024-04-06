fff323afd5bcab83235426b04d8fc4c7e592db62
Michał Sitkowski <emsitkowski@gmail.com>
2024-04-06 09:55:55 +0200

    feat: add sections to budget page

--- pages/budget.vue
+++ pages/budget.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>budget page</div>
+  <div>
+    <!-- Page header -->
+    <PageHeader
+      header-text="Monthly budget"
+      subheader-text="You can set monthly budget limits for each category"
+      :button="true"
+      button-text="Add category"
+      :button-icon="true"
+      button-icon-type="plus"
+      @button-click="isModalOpen = true"
+    />
+    <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4 sm:gap-8">
+      <CategoryCard :category="category" v-for="category in useCategoryStore().getAllCategories" />
+    </div>
+
+    <!-- Category modal -->
+    <CategoryActionsModal :open="isModalOpen" @close="isModalOpen = false" @success="isModalOpen = false" />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const isModalOpen = ref(false);
+</script>
 
 <style scoped></style>
