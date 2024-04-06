906797d3e95b32f11c5ac4d530a3d3204984fb5a
Michał Sitkowski <emsitkowski@gmail.com>
2024-04-06 09:57:14 +0200

    feat: add progress bar component

---
+++ components/ProgressBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col items-end">
+    <div class="relative w-full h-1 bg-primary-4% rounded-full overflow-hidden">
+      <div class="absolute top-0 left-0 h-full bg-primary-500" :style="`width: ${progress}%`"></div>
+    </div>
+    <span class="text-primary-500 mt-2 text-sm">{{ label }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps(["label", "progress"]);
+</script>
+
+<style scoped></style>
