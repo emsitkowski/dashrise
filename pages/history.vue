df4cb144ea3dd7c62231f59b7b2adcef8fc94ac5
Michał Sitkowski <emsitkowski@gmail.com>
2024-05-14 10:22:14 +0200

    feat: create history page

---
+++ pages/history.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- Page header -->
+    <PageHeader
+      header-text="History"
+      subheader-text="Choose date and check out the history of your transactions"
+      :button="true"
+      button-text="Add transaction"
+      :button-icon="true"
+      button-icon-type="plus"
+      :button-icon-leading="true"
+      @button-click="isModalOpen = true"
+    />
+
+    <!-- Transactions modal -->
+    <TransactionActionsModal :open="isModalOpen" @close="isModalOpen = false" @success="isModalOpen = false" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const isModalOpen = ref(false);
+</script>
+
+<style scoped></style>
