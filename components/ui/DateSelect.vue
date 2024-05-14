7c86b81dc5d4bd1ab8f7bb60c00fd12f16cc0560
Michał Sitkowski <emsitkowski@gmail.com>
2024-05-14 22:23:18 +0200

    style: adjust gap

--- components/ui/DateSelect.vue
+++ components/ui/DateSelect.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex gap-6 [&>*]:w-full max-w-md">
+  <div class="flex gap-4 sm:gap-6 [&>*]:w-full max-w-md">
     <SelectMenu
       :options="years"
       :default-selection="yearSelected"
