ad3560eb509792add1eb48a362629ac3ff8498b2
Michał Sitkowski <emsitkowski@gmail.com>
2024-05-16 13:24:18 +0200

    feat: install and set up floating vue library for popovers

---
+++ plugins/floating-vue.client.ts
@@ -0,0 +1,17 @@
+import FloatingVue from "floating-vue";
+import "floating-vue/dist/style.css";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(FloatingVue, {
+    themes: {
+      select: {
+        $resetCss: true,
+        triggers: ["click"],
+        autoHide: true,
+        placement: "bottom",
+        distance: 0,
+        skipTransition: true,
+      },
+    },
+  });
+});
