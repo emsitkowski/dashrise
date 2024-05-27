b9a05c904859bd0318cb3bdaf160beda11d961bb
Michał Sitkowski <emsitkowski@gmail.com>
2024-05-27 14:02:45 +0200

    refactor: adjust database template schema and permissions

--- database/schema.sql
+++ database/schema.sql
@@ -1,22 +1,9 @@
--- public."Categories" definition
-
--- Drop table
-
--- DROP TABLE public."Categories";
-
 CREATE TABLE public."Categories" (
 	id text NOT NULL,
 	created_at timestamptz DEFAULT (now() AT TIME ZONE 'cet'::text) NOT NULL,
 	"name" text NOT NULL,
 	CONSTRAINT "Categories_pkey" PRIMARY KEY (name)
 );
-
-
--- public."Transactions" definition
-
--- Drop table
-
--- DROP TABLE public."Transactions";
 
 CREATE TABLE public."Transactions" (
 	created_at timestamptz DEFAULT (now() AT TIME ZONE 'cet'::text) NOT NULL,
@@ -30,13 +17,6 @@ CREATE TABLE public."Transactions" (
 	CONSTRAINT "Transactions_pkey" PRIMARY KEY (id)
 );
 
-
--- public."UserCategories" definition
-
--- Drop table
-
--- DROP TABLE public."UserCategories";
-
 CREATE TABLE public."UserCategories" (
 	id uuid DEFAULT gen_random_uuid() NOT NULL,
 	user_id uuid DEFAULT auth.uid() NULL,
@@ -46,13 +26,37 @@ CREATE TABLE public."UserCategories" (
 );
 
 
--- public."Transactions" foreign keys
-
 ALTER TABLE public."Transactions" ADD CONSTRAINT "public_Transactions_category_fkey" FOREIGN KEY (category) REFERENCES public."Categories"("name") ON UPDATE CASCADE;
 ALTER TABLE public."Transactions" ADD CONSTRAINT "public_Transactions_user_id_fkey" FOREIGN KEY (user_id) REFERENCES auth.users(id);
-
-
--- public."UserCategories" foreign keys
-
 ALTER TABLE public."UserCategories" ADD CONSTRAINT "public_UserCategories_category_name_fkey" FOREIGN KEY ("name") REFERENCES public."Categories"("name") ON UPDATE CASCADE;
 ALTER TABLE public."UserCategories" ADD CONSTRAINT "public_UserCategories_user_id_fkey" FOREIGN KEY (user_id) REFERENCES auth.users(id);
+
+ALTER TABLE "Categories" enable row level security;
+ALTER TABLE "Transactions" enable row level security;
+ALTER TABLE "UserCategories" enable row level security;
+
+CREATE POLICY "Enable ALL actions for authenticated users based on user_id"
+on "public"."Transactions"
+to authenticated
+using (
+(auth.uid() = user_id )
+);
+
+CREATE POLICY "Enable ALL actions for authenticated users based on user_id"
+on "public"."UserCategories"
+to authenticated
+using (
+(auth.uid() = user_id )
+);
+
+CREATE POLICY "Enable INSERT actions for authenticated users based on user_id"
+on "public"."Categories"
+for INSERT
+to authenticated
+with check (true);
+
+CREATE POLICY "Enable SELECT actions for authenticated users"
+on "public"."Categories"
+for SELECT
+to authenticated
+using (true);
