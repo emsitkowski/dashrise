422fb4ec6ee792eae632b6f4e11e706663b4799d
Michał Sitkowski <emsitkowski@gmail.com>
2024-05-28 21:46:35 +0200

    feat: decrease app fade-in animation duration

--- app.vue
+++ app.vue
@@ -1,12 +1,12 @@
 <template>
   <div
     id="app"
-    class="relative after:content-[''] after:absolute after:w-full after:h-48 after:bg-gradient-to-t after:from-transparent after:to-primary-8% after:top-0 after:pointer-events-none after:select-none duration-1000"
+    class="relative after:content-[''] after:absolute after:w-full after:h-48 after:bg-gradient-to-t after:from-transparent after:to-primary-8% after:top-0 after:pointer-events-none after:select-none duration-500"
     :style="isAppLoading === true ? 'opacity: 0' : 'opacity: 1'"
   >
     <div class="container max-w-6xl min-h-screen mx-auto px-4">
       <NuxtLayout>
-        <div class="duration-200">
+        <div>
           <NuxtPage class="py-12 sm:py-20" />
         </div>
       </NuxtLayout>
@@ -25,6 +25,6 @@ onMounted(() => {
 
   setTimeout(() => {
     isAppLoading.value = false;
-  }, 500);
+  }, 100);
 });
 </script>
