8c66df26883eff33ab2852beb5fcce40746dc462
Michał Sitkowski <emsitkowski@gmail.com>
2024-05-30 15:56:14 +0200

    fix: disable body scrolling when scrollbar is disabled

--- utils/scrollbar.ts
+++ utils/scrollbar.ts
@@ -1,11 +1,13 @@
 function disableScrollbar() {
   const scrollBarWidth = window.innerWidth - document.documentElement.clientWidth;
   document.documentElement.classList.add("overflow-hidden");
+  document.body.classList.add("overflow-hidden");
   document.documentElement.style.paddingRight = scrollBarWidth + "px";
 }
 
 function enableScrollbar() {
   document.documentElement.classList.remove("overflow-hidden");
+  document.body.classList.remove("overflow-hidden");
   document.documentElement.style.paddingRight = "0px";
 }
 
