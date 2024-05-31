6c991e49ddb0e8931170c99b5deae525b17bc4bf
Michał Sitkowski <emsitkowski@gmail.com>
2024-05-31 21:31:12 +0200

    refactor: cleanup scrollbar control

--- utils/scrollbar.ts
+++ utils/scrollbar.ts
@@ -1,14 +1,26 @@
 function disableScrollbar() {
+  const documentElement = document.documentElement;
+  const body = document.body;
   const scrollBarWidth = window.innerWidth - document.documentElement.clientWidth;
-  document.documentElement.classList.add("overflow-hidden");
-  document.body.classList.add("overflow-hidden");
-  document.documentElement.style.paddingRight = scrollBarWidth + "px";
+
+  // disable scroll for html and body elements
+  documentElement.setAttribute("data-disable-scroll", "true");
+  body.setAttribute("data-disable-scroll", "true");
+
+  // adjust padding to prevent page shift
+  documentElement.style.paddingRight = scrollBarWidth + "px";
 }
 
 function enableScrollbar() {
-  document.documentElement.classList.remove("overflow-hidden");
-  document.body.classList.remove("overflow-hidden");
-  document.documentElement.style.paddingRight = "0px";
+  const documentElement = document.documentElement;
+  const body = document.body;
+
+  // enable scroll for html and body elements
+  documentElement.removeAttribute("data-disable-scroll");
+  body.removeAttribute("data-disable-scroll");
+
+  // restore initial padding value
+  documentElement.style.paddingRight = "0px";
 }
 
 export { disableScrollbar, enableScrollbar };
