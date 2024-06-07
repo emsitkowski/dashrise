23f9578a63f4918e699ef755d139734c91b5c86c
Michał Sitkowski <emsitkowski@gmail.com>
2024-06-07 15:49:58 +0200

    style: adjust styling for list overlay + button

--- components/widget/WidgetTransactions.vue
+++ components/widget/WidgetTransactions.vue
@@ -1,20 +1,28 @@
 <template>
-  <Card class="relative h-full min-h-[240px]">
+  <Card class="relative h-full min-h-[320px]">
     <template #card-body>
-      <div
-        class="relative h-full min-h-[320px] overflow-hidden after:content-[''] after:w-full after:h-full after:absolute after:bottom-0 after:left-0 after:bg-gradient-to-t after:from-white after:to-50% after:z-10 after:select-none after:pointer-events-none"
-        :class="{ 'after:content-none': $props.mode === 'history' }"
-      >
+      <div class="relative h-full overflow-hidden" :class="{ 'after:content-none': $props.mode === 'history' }">
         <div class="flex flex-col" v-if="transactions.length > 0">
+          <!-- Transactions list -->
           <TransactionList :transactions="transactions" />
-          <NuxtLink class="self-center" to="/history">
-            <Button
-              v-if="$props.mode !== 'history'"
-              class="absolute z-20"
-              label="See all transactions"
-              variant="primary-light"
-            />
-          </NuxtLink>
+
+          <!-- Show more overlay + button -->
+          <div
+            v-if="transactions.length > 3 && $props.mode !== 'history'"
+            class="absolute flex flex-col items-center bottom-0 w-full"
+          >
+            <div
+              class="after:content-[''] after:w-full after:h-[420px] after:absolute after:bottom-0 after:left-0 after:bg-gradient-to-t after:from-white after:from-5% after:to-100% after:z-10 after:select-none after:pointer-events-none"
+            ></div>
+            <NuxtLink to="/history">
+              <Button
+                v-if="$props.mode !== 'history'"
+                class="absolute z-20"
+                label="See all transactions"
+                variant="primary-light"
+              />
+            </NuxtLink>
+          </div>
         </div>
 
         <!-- No items found label -->
